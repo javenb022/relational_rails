5330e895b091fd0fa9c3ed1f71c7c2098075a27d
Javen Wilson <javenb022@gmail.com>
2023-05-10 16:34:28 -0400

    test for user story 1

---
+++ spec/features/garages/index_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "garages index page", type: :feature do
+  it "can see the name of each garage" do
+    garage_1 = Garage.create!(name: "Fester's",
+                              city: "Indianapolis",
+                              open_to_public: false,
+                              max_capacity: 6)
+    garage_2 = Garage.create!(name: "Golden's",
+                              city: "Zionsville",
+                              open_to_public: false,
+                              max_capacity: 8)
+    visit "/garages"
+
+    expect(page).to have_content(garage_1.name)
+    expect(page).to have_content(garage_2.name)
+  end
+end
