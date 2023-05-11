96b505f1342570a7878130f11361322c4ac00c4d
Javen Wilson <javenb022@gmail.com>
2023-05-10 22:16:49 -0400

    Add tests for garage attrubutes to be displayed

--- spec/features/garages/show_spec.rb
+++ spec/features/garages/show_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "garages show page" do
                               max_capacity: 6)
     garage_2 = Garage.create!(name: "Golden's",
                               city: "Zionsville",
-                              open_to_public: false,
+                              open_to_public: true,
                               max_capacity: 8)
     visit "/garages/#{garage_1.id}"
     expect(page).to have_content(garage_1.name)
@@ -22,15 +22,39 @@ RSpec.describe "garages show page" do
                               max_capacity: 6)
     garage_2 = Garage.create!(name: "Golden's",
                               city: "Zionsville",
-                              open_to_public: false,
+                              open_to_public: true,
                               max_capacity: 8)
     visit "/garages/#{garage_1.id}"
     expect(page).to have_content(garage_1.city)
     expect(page).to_not have_content(garage_2.city)
   end
 
-  it "says if the garage is open to the public"
+  it "says if the garage is open to the public" do
+    garage_1 = Garage.create!(name: "Fester's",
+                              city: "Indianapolis",
+                              open_to_public: false,
+                              max_capacity: 6)
+    garage_2 = Garage.create!(name: "Golden's",
+                              city: "Zionsville",
+                              open_to_public: true,
+                              max_capacity: 8)
+    visit "/garages/#{garage_1.id}"
+    expect(page).to have_content(garage_1.open_to_public)
+    expect(page).to_not have_content(garage_2.open_to_public)
+  end
 
-  it "shows the max capacity of the garage"
+  it "shows the max capacity of the garage" do
+    garage_1 = Garage.create!(name: "Fester's",
+                              city: "Indianapolis",
+                              open_to_public: false,
+                              max_capacity: 6)
+    garage_2 = Garage.create!(name: "Golden's",
+                              city: "Zionsville",
+                              open_to_public: false,
+                              max_capacity: 8)
+    visit "/garages/#{garage_1.id}"
+    expect(page).to have_content(garage_1.max_capacity)
+    expect(page).to_not have_content(garage_2.max_capacity)
+  end
 
 end
