802a263e7d0262597a8f2fbd3e821f5755a85d2b
Javen Wilson <javenb022@gmail.com>
2023-05-12 08:12:58 -0400

    Cars index page tests

---
+++ spec/features/cars/index_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+# User Story 3
+# As a visitor
+# When I visit '/child_table_name'
+# Then I see each Child in the system including the Child's attributes
+# (data from each column that is on the child table)
+RSpec.describe "cars index page" do
+  it "can see all the cars" do
+    garage = Garage.create!(name: "Fester's",
+                              city: "Indianapolis",
+                              open_to_public: false,
+                              max_capacity: 6)
+                              # require 'pry'; binding.pry
+    car1 = garage.cars.create!(make: "Honda", model: "Civic", year: 2019, electric: false)
+    car2 = garage.cars.create!(make: "Tesla", model: "Model S", year: 2020, electric: true)
+
+    visit "/cars"
+    expect(page).to have_content(car1.make)
+    expect(page).to have_content(car1.model)
+    expect(page).to have_content(car1.year)
+    expect(page).to have_content(car1.electric)
+    expect(page).to have_content(car2.make)
+    expect(page).to have_content(car2.model)
+    expect(page).to have_content(car2.year)
+    expect(page).to have_content(car2.electric)
+  end
+end
+
+# Create 2 car instances, will have to migrate the car table
+# create a route, create controller and then index view
