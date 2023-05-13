7a1ed5d01c84b047fd5b214a4ac26811b480032d
Javen Wilson <javenb022@gmail.com>
2023-05-13 16:31:52 -0400

    Test for order by created method

--- spec/models/garage_spec.rb
+++ spec/models/garage_spec.rb
@@ -8,8 +8,14 @@ RSpec.describe Garage, type: :model do
       garage_1 = Garage.create!(name: "Fester's",
                                 city: "Indianapolis",
                                 open_to_public: false,
-                                max_capacity: 6)
-      expect(garage_1.created).to eq(garage_1.created_at)
+                                max_capacity: 6,
+                                created_at: Date.today - 1)
+      garage_2 = Garage.create!(name: "Golden's",
+                                city: "Zionsville",
+                                open_to_public: true,
+                                max_capacity: 8)
+
+      expect(Garage.order_by_created).to eq([garage_2, garage_1])
     end
   end
 end
