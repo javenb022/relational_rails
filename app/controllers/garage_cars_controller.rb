95e5c32ef64be5060cad1135669b2e27feb38013
Javen Wilson <javenb022@gmail.com>
2023-05-16 18:09:49 -0400

    Add routes and actions for new and create

--- app/controllers/garage_cars_controller.rb
+++ app/controllers/garage_cars_controller.rb
@@ -3,4 +3,20 @@ class GarageCarsController < ApplicationController
     @garage = Garage.find(params[:garage_id])
     @cars = @garage.cars.all
   end
+
+  def new
+    @garage = Garage.find(params[:garage_id])
+  end
+
+  def create
+    @garage = Garage.find(params[:garage_id])
+    car = @garage.cars.create!(car_params)
+    redirect_to "/garages/#{@garage.id}/cars"
+  end
+
+  private
+
+  def car_params
+    params.permit(:make, :model, :electric, :year)
+  end
 end
